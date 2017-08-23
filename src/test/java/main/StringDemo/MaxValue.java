a5ac1ab5c69180ea2c443b9bd24c50a4b4c2200d
Ravi Kuntal <kuntalravi91@.com>
2017-08-23 22:18:47 +0530

    ffhgjhg

---
+++ src/test/java/main/StringDemo/MaxValue.java
@@ -0,0 +1,21 @@
+package test.java.main.StringDemo;
+
+import java.util.HashMap;
+
+public class MaxValue 
+{
+	static String s = new String("geeksforgeeks");
+	static char[] ch = s.toCharArray();
+	public static void main(String[] args) 
+	{
+		int count=0;
+		int max=-1;
+		for(int i=0; i<ch.length; i++)
+		{
+			for(int j=1; j<ch.length; j++)
+			{
+				
+			}
+		}
+	}
+}
---
+++ src/test/java/print/number/PrintNumber.java
@@ -0,0 +1,13 @@
+package test.java.print.number;
+
+public class PrintNumber {
+	
+	String[] arr1 = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+	String[] arr2 = {"twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninty"};
+	String[] arr3 = {"hundred", "thousand", "lac", "caror"};
+	
+	public static void main(String[] args) {
+		
+		
+	}
+}
